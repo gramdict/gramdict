fd1ddc027e256453674e57c0c3d61966bb9a6a33
Matt Trussler <meberem@gmail.com>
2019-02-14 17:11:46 +0000

    use history api to persist and load search terms

---
+++ gramdictru/Pages/search.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class searchModel : PageModel
+    {
+        public string Term { get; set; }
+
+        public void OnGet(string term)
+        {
+            Term = term;
+        }
+    }
+}
