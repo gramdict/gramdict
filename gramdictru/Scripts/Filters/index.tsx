334662462b1be671e05e7156bfd15b23a9fda878
sergey-zakharoff <50071892+sergey-zakharoff@users.noreply.github.com>
2019-05-12 13:53:09 +0300

    #54 Add tooltips to filters (#55)

--- gramdictru/Scripts/Filters/index.tsx
+++ gramdictru/Scripts/Filters/index.tsx
@@ -1,4 +1,5 @@
 ﻿import * as React from "react";
+import ReactTooltip from "react-tooltip";
 import { ApplicationState } from "../ApplicationState";
 import { observer } from "mobx-react";
 
@@ -22,18 +23,61 @@ class Filter extends React.Component<IFilterControlProps & IFilterProps> {
     }
 }
 
-const possibleFilters = [
-    ["м", "мо", "мо-жо", "союз"],
-    ["ж", "жо", "мн.", "межд."],
-    ["с", "со", "мн. <i>от</i>", "предл."],
-    ["н", "мс", "числ.", "предик."],
-    ["п", "мс-п", "числ.-п", "част."],
-    ["св", "нсв", "св-нсв"],
+const possibleFilters: { name: string, tooltip: string, link: number }[][] = [
+    [
+        { name: "м", tooltip: "существительное мужского рода, неодушевленное", link: 0},
+        { name: "мо", tooltip: "существительное мужского рода, одушевленное", link: 0},
+        { name: "мо-жо", tooltip: "существительное мужского или женского рода, одушевленное", link: 0},
+        { name: "союз", tooltip: "союз", link: 0}
+    ],[
+        { name: "ж", tooltip: "существительное женского рода, неодушевленное", link: 0},
+        { name: "жо", tooltip: "существительное женского рода, одушевленное", link: 0},
+        { name: "мн.", tooltip: "существительное, имеющее только множественное число", link: 0},
+        { name: "межд.", tooltip: "междометие", link: 0}
+    ],[
+        { name: "с", tooltip: "существительное среднего рода, неодушевленное", link: 0},
+        { name: "со", tooltip: "существительное среднего рода, одушевленное", link: 0},
+        { name: "мн. <i>от</i>", tooltip: "существительное, имеющее только множественное число, образованное от обычного существительного", link: 0},
+        { name: "предл.", tooltip: "предлог", link: 0}
+    ],[
+        { name: "н", tooltip: "наречие", link: 0},
+        { name: "мс", tooltip: "местоимение", link: 0},
+        { name: "числ.", tooltip: "числительное", link: 0},
+        { name: "предик.", tooltip: "предикатив", link: 0}
+    ],[
+        { name: "п", tooltip: "прилагательное", link:0},
+        { name: "мс-п", tooltip: "местоимение-прилагательное", link: 0},
+        { name: "числ.-п", tooltip: "порядковое числительное", link: 0},
+        { name: "част.", tooltip: "частица", link: 0}
+    ],[
+        { name: "св", tooltip: "глагол совершенного вида", link: 1},
+        { name: "нсв", tooltip: "глагол несовершенного вида", link: 1},
+        { name: "св-нсв", tooltip: "двувидовой глагол", link: 1}
+    ]
 ];
 
-const formatLabel = (filter: string) => {
-    return <span dangerouslySetInnerHTML={{ __html: filter }} />;
+const linksToAgenda = [
+    ['(буквенные символы)','/declension/symbols#main-symbol'],
+    ['(буквенные символы глаголов)','/conjugation#verb-symbol']
+];
+
+const formatLabelPart = (filter) => {
+    return <span data-tip={filter.tooltip} data-for={filter.name} dangerouslySetInnerHTML={{ __html: filter.name }}/>;
+};
+
+const formatLabel = (filter) => {
+    return [formatLabelPart(filter),
+        <ReactTooltip type="light" effect="float" id={filter.name} border={true} clickable={true} getContent={(dataTip) =>
+            [
+                <div className="tooltipClass">
+                    <span className="tooltipClass">{dataTip}&nbsp;</span>
+                    <br/>
+                    <a href={linksToAgenda[filter.link][1]}>{linksToAgenda[filter.link][0]}</a>
+                </div>
+        ]} />
+    ];
 }
+
 
 @observer
 export class Filters extends React.Component<IFilterControlProps> {
@@ -41,11 +85,14 @@ export class Filters extends React.Component<IFilterControlProps> {
         return [
             <table className="filter-table">
                 <tbody>
-                {possibleFilters.map(row => <tr>{row.map(f => <Filter filter={f} applicationState={this.props.applicationState}>
-                    {formatLabel(f)}
+                    {possibleFilters.map(row => <tr>{row.map(item => <Filter filter={item.name} applicationState={this.props.applicationState}>
+                    {formatLabel(item)}
                 </Filter>)}</tr>)}
                </tbody>
             </table>,
             <div className="reset-filter-button" onClick={() => this.props.applicationState.resetFilters()}>Reset</div>];
     }
 }
+
+
+
