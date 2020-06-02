fd67784200bda92e5563d26d2d46e004348e18aa
GoToNightmare <holdscroll@gmail.com>
2020-06-02 10:19:08 +0500

    1

--- gramdictru/Startup.cs
+++ gramdictru/Startup.cs
@@ -56,7 +56,7 @@ namespace gramdictru
 
         static readonly HttpClient ApiClient = new HttpClient()
         {
-            BaseAddress = new Uri("http://api.gramdict.ru")
+            BaseAddress = new Uri("https://localhost:44328")
         };
     }
 }
