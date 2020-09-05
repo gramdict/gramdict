72eec22cfbf6f8bc3a9dd9f18fe024a265534ba7
Sergey Slepov <slepov@gmail.com>
2020-09-05 15:42:13 +0100

    Add UseWellKnownDir

---
+++ gramdictru/LetsEncryptExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+
+namespace gramdictru
+{
+    static class LetsEncryptExtensions
+    {
+        public static void UseWellKnownDir(this IApplicationBuilder app)
+        {
+            string wellKnownDir = Path.Combine(Directory.GetCurrentDirectory(), @".well-known");
+            Directory.CreateDirectory(wellKnownDir); // noop if dir exists
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(wellKnownDir),
+                RequestPath = new PathString("/.well-known"),
+                ServeUnknownFileTypes = true // serve extensionless file
+            });
+        }
+    }
+}
--- gramdictru/Startup.cs
+++ gramdictru/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.Webpack;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace gramdictru
@@ -45,6 +44,8 @@ namespace gramdictru
             });
 
             app.UseStaticFiles();
+
+            app.UseWellKnownDir(); // required for Let's Encrypt certificate renewal
 
             app.UseMvc(routes =>
             {
