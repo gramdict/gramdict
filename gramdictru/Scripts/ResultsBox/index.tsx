d710fcf5da4855ca21d040d11bfb11695321e0f3
hoangmeo <hoangn.meo@gmail.com>
2023-02-14 12:22:48 +0700

    [#191] Copying dictionary entries should not add line breaks

--- gramdictru/Scripts/ResultsBox/index.tsx
+++ gramdictru/Scripts/ResultsBox/index.tsx
@@ -16,15 +16,15 @@ export class ResultsBox extends React.Component<IResultBoxProps> {
     render() {
         return this.props.applicationState.hasSearched &&
             <div className="page is-searching" id="search-results">
-            <AnimateHeight className="static-filters"
-                duration={this.props.applicationState.nextAnimationTime}
-                height={this.props.applicationState.nextAnimationHeight}
-                onAnimationEnd={val => this.props.applicationState.ensureFilterAreClosed()}
-                style={{
-                backgroundImage: "url('../background.jpg')"
-        }}>
-                <Filters applicationState={this.props.applicationState} />
-            </AnimateHeight>
+                <AnimateHeight className="static-filters"
+                    duration={this.props.applicationState.nextAnimationTime}
+                    height={this.props.applicationState.nextAnimationHeight}
+                    onAnimationEnd={val => this.props.applicationState.ensureFilterAreClosed()}
+                    style={{
+                        backgroundImage: "url('../background.jpg')"
+                    }}>
+                    <Filters applicationState={this.props.applicationState} />
+                </AnimateHeight>
                 <div className="body-content">
                     <InfinityScroll
                         dataLength={this.props.applicationState.total}
@@ -38,25 +38,32 @@ export class ResultsBox extends React.Component<IResultBoxProps> {
                             overflow: "hidden"
                         }}
                         key={this.props.applicationState.searchedTerm}>
-                        {this.props.applicationState.results.map(resultSet => [
-                            <div className={"results-table" + (this.props.applicationState.isShortResult ? " short-results" : "")}>
-                                {resultSet.map(r => <div className="result-entry">
-                        <div className="lemma" style={{textAlign: this.props.applicationState.searchTerm === "" || this.props.applicationState.searchTerm.startsWith('*') ? 'right' : 'left'}}>{r.lemma}</div>
-                        <div className="symbol" dangerouslySetInnerHTML={{ __html: r.symbol }} />
-                        <div className="grammar" dangerouslySetInnerHTML={{ __html: r.grammar }} />
-                    </div>)}
+                        {this.props.applicationState.results.map((resultSet, index1) => [
+                            <div className={"results-table" + (this.props.applicationState.isShortResult ? " short-results" : "")} key={index1}>
+                                {resultSet.map((r, index2) => <div className="result-entry" key={index2}>
+                                    <div className="lemma" style={{ textAlign: this.props.applicationState.searchTerm === "" || this.props.applicationState.searchTerm.startsWith('*') ? 'right' : 'left' }}>
+                                        <label className='entry-hidden'>__</label>
+                                        {r.lemma}
+                                        <label className='entry-hidden'>__&nbsp;</label>
+                                    </div>
+                                    <div className="symbol" dangerouslySetInnerHTML={{ __html: r.symbol }} />
+                                    <div className="grammar"  >
+                                        <label className='entry-hidden'>&nbsp;</label>
+                                        <span dangerouslySetInnerHTML={{ __html: r.grammar }} />
+                                    </div>
+                                </div>)}
                             </div>,
-                            <hr/>
+                            <hr />
                         ])}
-                    {this.props.applicationState.canLoadMore &&
-                        <div className="load-more">
-                            <a href="javascript:void(0)" onClick={() => {
-                                this.props.applicationState.continue();
-                                this.loader.setState({
-                                    showLoader: true,
-                                });
-                        }}>Load more...</a>
-                        </div>}
+                        {this.props.applicationState.canLoadMore &&
+                            <div className="load-more">
+                                <a href="javascript:void(0)" onClick={() => {
+                                    this.props.applicationState.continue();
+                                    this.loader.setState({
+                                        showLoader: true,
+                                    });
+                                }}>Load more...</a>
+                            </div>}
                     </InfinityScroll>
                 </div>
             </div>;
